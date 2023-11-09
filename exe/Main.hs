ed1e9ae4a11559ccbe47ccdd065190a4a10ce69b
Sander <hey@sandydoo.me>
2023-11-09 12:47:37 +0000

    Add a command to restore/unpack a NAR

--- exe/Main.hs
+++ exe/Main.hs
@@ -1,20 +1,74 @@
+{-# LANGUAGE ApplicativeDo #-}
+{-# LANGUAGE RecordWildCards #-}
+
 module Main where
 
-import System.Directory
-import System.Environment
-import System.FilePath
+import Options.Applicative
 import System.IO
 import System.Nix.Nar
 
+newtype Options = Options Command
+
+data Command
+  = DumpNar DumpNarOptions
+  | RestoreNar RestoreNarOptions
+
+data DumpNarOptions = DumpNarOptions
+  { dumpStorePath :: FilePath,
+    dumpOutPath :: FilePath
+  }
+
+data RestoreNarOptions = RestoreNarOptions
+  { restoreNarPath :: FilePath,
+    restoreOutPath :: FilePath
+  }
+
+optionsParser :: Parser Options
+optionsParser = Options <$> commandParser
+
+commandParser :: Parser Command
+commandParser =
+  subparser $
+    mconcat
+      [ command "dump" (info (DumpNar <$> dumpNarOptionsParser) (progDesc "Dump the NAR for a store path")),
+        command "restore" (info (RestoreNar <$> restoreNarOptionsParser) (progDesc "Restore a NAR to a directory"))
+      ]
+
+dumpNarOptionsParser :: Parser DumpNarOptions
+dumpNarOptionsParser = do
+  dumpStorePath <- argument str (metavar "STOREPATH")
+  dumpOutPath <- option str (long "out" <> short 'o' <> metavar "OUTPATH")
+  pure $ DumpNarOptions {..}
+
+restoreNarOptionsParser :: Parser RestoreNarOptions
+restoreNarOptionsParser = do
+  restoreNarPath <- argument str (metavar "NARPATH")
+  restoreOutPath <- option str (long "out" <> short 'o' <> metavar "OUTPATH")
+  pure $ RestoreNarOptions {..}
+
 main :: IO ()
 main = do
-  [storePath] <- getArgs
+  Options cmd <- execParser $ info (optionsParser <**> helper) fullDesc
 
-  pwd <- getCurrentDirectory
-  let narFilePath = pwd </> "nar.dump"
+  case cmd of
+    DumpNar opts -> dumpNar opts
+    RestoreNar opts -> restoreNar opts
 
+dumpNar :: DumpNarOptions -> IO ()
+dumpNar DumpNarOptions {..} = do
   -- Create a new file and write the nar to it
-  withBinaryFile narFilePath WriteMode $ \h -> do
-    buildNarIO narEffectsIO storePath h
+  withBinaryFile dumpOutPath WriteMode $ \h -> do
+    buildNarIO narEffectsIO dumpStorePath h
 
-    putStrLn $ "Wrote nar to " ++ narFilePath
+    putStrLn $ "Wrote nar to " ++ dumpOutPath
+
+restoreNar :: RestoreNarOptions -> IO ()
+restoreNar RestoreNarOptions {..} = do
+  res <- withBinaryFile restoreNarPath ReadMode $ \h ->
+    runParser
+      narEffectsIO
+      parseNar
+      h
+      restoreOutPath
+
+  print res
